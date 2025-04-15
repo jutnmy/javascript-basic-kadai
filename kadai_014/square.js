94aba1b607c0885e12038d6ace4e83872dbc8be1
jutnmy <89903647+jutnmy@users.noreply.github.com>
2025-04-15 14:07:09 +0900

    Add files via upload

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -5,10 +5,17 @@
 // getSquare(10);
 
 // 以下再提出
-let num = 10;
+// let num = 10;
 
-const getSquare = () => {
+// const getSquare = () => {
+//   return num ** 2;
+// }
+
+// console.log(getSquare());
+
+// 以下再々提出
+const getSquare = (num) => {
   return num ** 2;
 }
 
-console.log(getSquare());
+console.log(getSquare(10));
